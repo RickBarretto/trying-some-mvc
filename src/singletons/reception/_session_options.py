5f284bb2c6fb00630d3f3a8123fa457aab610d92
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2024-01-21 16:36:44 -0300

    [SessionOptions\find_session] improve function

--- src/singletons/reception/_session_options.py
+++ src/singletons/reception/_session_options.py
@@ -54,13 +54,33 @@ class SessionOptions:
 
     @staticmethod
     def find_session(clinic: clinic.ClinicController):
+        """Registra uma nova sessão no banco de dados.
+
+        Questions
+        ---------
+        * Data da sessão
+
+        Feedbacks
+        ---------
+        * Informações básicas da sessão
+
+        Warnings
+        --------
+        * Formato de data inválido
+        * Sessão não registrada
+        """
+
+        # Valida entrada
         try:
             date = screen.Prompt().get_date()
         except ValueError as e:
             screen.WarningScreen(e).render()
             return
+        
+        # Valida entrada
+        session = clinic.find_session(date)
 
-        if clinic.find_session(date):
-            screen.WarningScreen("Sessão encontrada.").render()
+        if session:
+            screen.WarningScreen(["Sessão encontrada.", session.formated_date, str(session.status)]).render()
         else:
             screen.WarningScreen("Sessão não encontrada.").render()
