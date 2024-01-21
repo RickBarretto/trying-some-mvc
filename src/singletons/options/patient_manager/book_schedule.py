40d7606ef38999d3b6ea5d145eee28333772b55d
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2024-01-21 16:12:34 -0300

    move `book_schedule` to a separated module

---
+++ src/singletons/options/patient_manager/book_schedule.py
@@ -0,0 +1,59 @@
+
+from entities.clinic import ClinicController
+from screen.prompt import Prompt
+from screen.warning import WarningScreen
+
+
+def book_schedule(clinic: ClinicController):
+    """Agenda uma sessão para um paciente.
+
+    Questions
+    ---------
+    * CPF do paciente
+    * Data da sessão
+
+    Feedbacks
+    ---------
+    * Status do agendamento
+
+    Warnings
+    --------
+    * Formato de data inválido
+    * Formato de CPF inválido
+    * Paciente não registrado
+    * Sessão não registrada
+    * Sessão já finalizada
+    * Paciente já estava agendado
+    """
+
+    # Valida entradas
+    try:
+        patient_cpf = Prompt.get_cpf()
+        session_date = Prompt.get_date()
+    except ValueError as e:
+        WarningScreen(e).render()
+        return
+
+    patient = clinic.find_patient(patient_cpf)
+    session = clinic.find_session(session_date)
+
+    # Valida registros
+    if not patient:
+        WarningScreen("Paciente não registrado.").render()
+        return
+
+    if not session:
+        WarningScreen("Sessão não registrada.").render()
+        return
+
+    # Verifica o agendamento
+    if session.uid in patient.scheduled_sessions:
+        WarningScreen(
+            f"Paciente {patient.name} já está "
+            f"registrado para a sessão {session.formated_date}."
+        ).render()
+        return
+
+    # Agenda sessão para paciente
+    clinic.book_schedule(patient, session)
+    WarningScreen("Paciente agendado com sucesso!").render()
--- src/singletons/reception/_patient_options.py
+++ src/singletons/reception/_patient_options.py
@@ -5,61 +5,6 @@ import screen
 class PatientOptions:
 
     @staticmethod
-    def book_schedule(clinic: clinic.ClinicController):
-        """Agenda uma sessão para um paciente.
-
-        Questions
-        ---------
-        * CPF do paciente
-        * Data da sessão
-
-        Feedbacks
-        ---------
-        * Status do agendamento
-
-        Warnings
-        --------
-        * Formato de data inválido
-        * Formato de CPF inválido
-        * Paciente não registrado
-        * Sessão não registrada
-        * Sessão já finalizada
-        * Paciente já estava agendado
-        """
-
-        # Valida entradas
-        try:
-            patient_cpf = screen.Prompt.get_cpf()
-            session_date = screen.Prompt.get_date()
-        except ValueError as e:
-            screen.WarningScreen(e).render()
-            return
-
-        patient = clinic.find_patient(patient_cpf)
-        session = clinic.find_session(session_date)
-
-        # Valida registros
-        if not patient:
-            screen.WarningScreen("Paciente não registrado.").render()
-            return
-
-        if not session:
-            screen.WarningScreen("Sessão não registrada.").render()
-            return
-
-        # Verifica o agendamento
-        if session.uid in patient.scheduled_sessions:
-            screen.WarningScreen(
-                f"Paciente {patient.name} já está "
-                f"registrado para a sessão {session.formated_date}."
-            ).render()
-            return
-
-        # Agenda sessão para paciente
-        clinic.book_schedule(patient, session)
-        screen.WarningScreen("Paciente agendado com sucesso!").render()
-
-    @staticmethod
     def list_patient_bookings(clinic: clinic.ClinicController):
         """Lista os agendamentos de um paciente.
 
