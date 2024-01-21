e392a8e48a9fcdc49b2d0696c013deb80ef685a6
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2024-01-21 15:43:53 -0300

    move `check_current_booking` to `options` module

---
+++ src/singletons/options/current_session_manager/check_booking.py
@@ -0,0 +1,43 @@
+from entities.clinic import ClinicController
+
+from screen.prompt import Prompt
+from screen.splash import SplashScreen
+from screen.warning import WarningScreen
+
+
+def check_current_booking(clinic: ClinicController):
+    """Verifica se paciente está agendado para a sessão atual.
+    
+    Questions
+    ---------
+    * CPF do paciente
+
+    Feedbacks
+    ---------
+    * Status do agendamento
+
+    Warnings
+    --------
+    * Paciente não registrado
+    """
+
+    # Entrada do CPF
+    try:
+        cpf = Prompt.get_cpf()
+    except ValueError as e:
+        return WarningScreen(e).render()
+
+    patient = clinic.find_patient(cpf)
+
+    # Verifica se paciente existe no banco de dados
+    if not patient:
+        return WarningScreen("Paciente não registrado.").render()
+
+    # Verifica se paciente está marcado para a sessão atual
+    is_booked = clinic.model.current_session.uid in patient.scheduled_sessions
+
+    # Imprime o feedback
+    TEMPLATE_MESSAGE = "Paciente {} está marcado para a sessão atual."
+    message = TEMPLATE_MESSAGE.format("" if is_booked else "não")
+
+    SplashScreen(message).render()
