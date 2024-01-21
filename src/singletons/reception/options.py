7c92de91fc4bb8a352e4f4c6859f69d0f34523cd
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2024-01-21 15:56:47 -0300

    remove unused import

--- src/singletons/reception/options.py
+++ src/singletons/reception/options.py
@@ -3,8 +3,6 @@ from entities import clinic
 
 from ._session_options import SessionOptions
 from ._patient_options import PatientOptions
-from ._current_session_option import CurrentSessionOptions
 
-
-class ReceptionOptions(SessionOptions, PatientOptions, CurrentSessionOptions):
+class ReceptionOptions(SessionOptions, PatientOptions):
     pass
