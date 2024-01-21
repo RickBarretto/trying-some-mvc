e29dbcbc4d059b5063fbc4afb7edb961ec92da15
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2024-01-21 15:45:54 -0300

    use __init__ as en exporter and namespace for internal functions

---
+++ src/singletons/options/current_session_manager/__init__.py
@@ -0,0 +1,9 @@
+from .check_booking import check_current_booking
+from .next_patient import  show_next_patient
+from .waiting_queue import send_to_waiting_queue
+
+__all__ = [
+    "check_current_booking"
+    "show_next_patient"
+    "send_to_waiting_queue"
+]
