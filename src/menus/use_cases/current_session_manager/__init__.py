1fbbb55fddfcd170208ed568c386c55db576b7f0
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2024-01-21 19:38:14 -0300

    rename to a shorter name: `start_session` -> `start`

--- src/menus/use_cases/current_session_manager/__init__.py
+++ src/menus/use_cases/current_session_manager/__init__.py
@@ -1,5 +1,5 @@
 from .check_booking import check_current_booking
 from .next_patient import show_next_patient
 from .waiting_queue import send_to_waiting_queue
-from .start_session import start
+from .start import start
 from .update import update
