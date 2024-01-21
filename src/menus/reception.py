fa8a833f5cd9e8ac2f3d0bf5377e26a37c63fab8
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2024-01-21 17:01:34 -0300

    [reception] update whole module

--- src/menus/reception.py
+++ src/menus/reception.py
@@ -1,34 +1,38 @@
-from typing import Callable
-from tui.main_menu import Controller, MainMenu
-from entities import clinic
+"""Define um Singleton para `reception`
+
+Basicamente só pode existir uma única instância de `reception`,
+e por consequência do controlador (model e view) que o mesmo possui.
+
+Assim, é exposto uma única instância `reception`, 
+ao invés da declaração de classe,
+podendo assim ser usado por outros módulos.
+"""
+
+
+from tui.main_menu import MainMenu
+from entities.clinic import ClinicController, ClinicModel, ClinicView
 
 from .options import current_session_manager
 from .options import patient_manager
 from .options import session_manager
 
-__all__ = ["reception"]
+# Todas as opções disponíveis para a recepção
 
-reception = MainMenu(
-    clinic.ClinicController(clinic.ClinicModel(), clinic.ClinicView()),
-    [
-        (session_manager.register, "Registrar nova sessão."),
-        (session_manager.list_all, "Listar sessões registradas."),
-        (session_manager.find, "Procurar sessão por data."),
-        (current_session_manager.start_current_session, "Iniciar sessão atual."),
-        (patient_manager.register, "Registrar novo paciente."),
-        (patient_manager.book_schedule, "Agendar sessão para paciente."),
-        (
-            patient_manager.list_bookings,
-            "Listar sessões agendadas para paciente.",
-        ),
-        (
-            current_session_manager.check_current_booking,
-            "Verificar agendamento para sessão atual.",
-        ),
-        (
-            current_session_manager.send_to_waiting_queue,
-            "Enviar paciente para fila de espera.",
-        ),
-        (current_session_manager.show_next_patient, "Mostrar próximo paciente."),
-    ],
-)
+options = [
+    (session_manager.register, "Registrar nova sessão."),
+    (session_manager.list_all, "Listar sessões registradas."),
+    (session_manager.find, "Procurar sessão por data."),
+    (current_session_manager.start_current_session, "Iniciar sessão atual."),
+    (patient_manager.register, "Registrar novo paciente."),
+    (patient_manager.book_schedule, "Agendar sessão para paciente."),
+    (patient_manager.list_bookings,"Listar sessões agendadas para paciente.",),
+    (current_session_manager.check_current_booking, "Verificar agendamento para sessão atual.",),
+    (current_session_manager.send_to_waiting_queue,"Enviar paciente para fila de espera.",),
+    (current_session_manager.show_next_patient, "Mostrar próximo paciente."),
+]
+
+controller = ClinicController(ClinicModel(), ClinicView())
+
+reception = MainMenu(controller,options)
+
+__all__ = ["reception"]
