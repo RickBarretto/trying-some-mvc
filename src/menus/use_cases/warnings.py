625ce22474bc2b790579b17bc74093be009b4c4e
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2024-01-22 20:40:54 -0300

    add session not registered warning

--- src/menus/use_cases/warnings.py
+++ src/menus/use_cases/warnings.py
@@ -4,6 +4,8 @@ import tui
 def patient_not_registered(patient_cpf: str):
     tui.warn(f"Não há paciente registrado para CPF {patient_cpf}.")
 
+def session_not_registered(session_date: str):
+    tui.warn(f"Não há sessão registrada para o dia {session_date}")
 
 def session_has_already_started(session: Session):
     tui.warn(f"A sessão {session.date} já foi iniciada!")
