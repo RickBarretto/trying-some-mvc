8c80f3bc451bec186ecfdad284953c9bfdd6cfa4
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2024-01-22 20:29:30 -0300

    create session status's warnings

--- src/menus/use_cases/warnings.py
+++ src/menus/use_cases/warnings.py
@@ -1,5 +1,11 @@
+from entities.session import Session
 import tui
 
 def patient_not_registered(patient_cpf: str):
     tui.warn(f"Paciente não registrado para CPF {patient_cpf}.")
 
+def session_was_never_started(session: Session):
+    tui.warn(f"A sessão {session.date} nunca foi iniciada!")
+
+def session_already_was_finished(session: Session):
+    tui.warn(f"A sessão {session.date} já foi finalizada!")
