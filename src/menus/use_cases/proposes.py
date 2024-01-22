61944ec296d3eeea43ee14b00b7e6fcaf550efec
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2024-01-22 17:18:24 -0300

    create the proposes module

---
+++ src/menus/use_cases/proposes.py
@@ -0,0 +1,51 @@
+
+
+from typing import Callable
+from entities.clinic import Clinic
+from entities.patient import Patient
+from entities.session import Session
+from menus.use_cases import current_session_manager, patient_manager, session_manager
+import tui
+
+
+def wish_add_to_waiting_queue(clinic: Clinic, patient: Patient) -> bool:
+    if tui.progress(f"Desejas adicionar {patient.name} à fila de espera?"):
+        current_session_manager.send_to_waiting_queue(clinic, patient)
+        return True
+    
+    return False
+
+def wish_send_patient_to_waiting_queue(clinic: Clinic, patient: Patient) -> bool:
+    if tui.progress(f"Desejas encaminhar {patient.name} para fila de espera?"):
+        current_session_manager.send_to_waiting_queue(clinic, patient)
+        return True
+    
+    return False
+
+def wish_start_current_session(clinic: Clinic) -> bool:
+    if tui.progress(f"Desejas iniciar sessão atual?"):
+        current_session_manager.start(clinic)
+        return True
+    
+    return False
+
+def wish_register_patient(clinic: Clinic, patient_cpf: str) -> bool:
+    if tui.progress(f"Desejas registar paciente do CPF {patient_cpf}?"):
+        patient_manager.register(clinic, patient_cpf)
+        return True
+    
+    return False
+
+def wish_book_session(clinic: Clinic, patient: Patient, session: Session) -> bool:
+    if tui.progress(f"Desejas agendar {patient.name} para {session.date}?"):
+        patient_manager.book_schedule(clinic, patient, session)
+        return True
+    
+    return False
+
+def wish_register_session(clinic: Clinic, session_date: str) -> bool:
+    if tui.progress(f"Desejas registar sessão na data {session_date}?"):
+        session_manager.register(clinic, date=session_date)
+        return True
+    
+    return False
