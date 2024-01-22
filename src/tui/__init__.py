e071c2054f38267f28e59e55fcfb1ef416165bf4
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2024-01-22 19:08:56 -0300

    make prompt available

--- src/tui/__init__.py
+++ src/tui/__init__.py
@@ -3,10 +3,10 @@
 As telas serão usadas pelas classes de ``src.singleton``.
 """
 
-from . import prompt
 from .choice import choice
 from .info import info
 from .list import bullet_list
 from .progress import progress
+from .prompt import prompt
 from .splash import splash
 from .warning import warn
