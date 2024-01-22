ecbe2fd8463016155a7be99df26e98ac25e2a1bf
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2024-01-22 15:52:36 -0300

    accept external calls

--- src/menus/use_cases/patient_manager/register.py
+++ src/menus/use_cases/patient_manager/register.py
@@ -7,7 +7,7 @@ import tui
 from menus.use_cases import status
 
 
-def register(clinic: Clinic) -> bool:
+def register(clinic: Clinic, patient_cpf: str | None = None) -> bool:
     """Registra paciente no banco de dados.
 
     Questions
@@ -36,21 +36,22 @@ def register(clinic: Clinic) -> bool:
 
     # Valida entradas
     try:
-        cpf: str = prompt.get_cpf()
-        name: str = prompt.prompt("Insira o nome do paciente.")
-        extra: str = prompt.multiline("Insira informações extra.")
+        if patient_cpf is None:
+            patient_cpf: str = prompt.get_cpf()
+        patient_name: str = prompt.prompt("Insira o nome do paciente.")
+        extra_information: str = prompt.multiline("Insira informações extra.")
     except ValueError as e:
         tui.warn(e)
         return status.MayBeRepeated
 
     # Verifica se paciente já está registrado
-    if clinic.patient_by_cpf(cpf):
+    if clinic.patient_by_cpf(patient_cpf):
         tui.warn("Paciente já foi registrado.")
         return status.Ok
 
     # Criação do paciente
     new_id: int = clinic.new_patient_id()
-    patient: Patient = Patient(uid=new_id, cpf=cpf, name=name, extra_info=extra)
+    patient: Patient = Patient(uid=new_id, cpf=patient_cpf, name=patient_name, extra_info=extra_information)
 
     # Registro do paciente
     clinic.patients.append(patient)
