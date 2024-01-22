b3194978ed60a9ead8e8ce87890efddb1bc8e5d8
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2024-01-22 19:08:39 -0300

    minor improvement

--- src/tui/prompt.py
+++ src/tui/prompt.py
@@ -30,63 +30,13 @@ def prompt(ask: str, suggestion: str = "") -> str:
     screen.render_content(content, center=True)
     screen.render_vertical_space(height=content_height)
     screen.render_rule(position=2)
-    return input(" >>> ")
+    return input(" >>> ").strip()
 
 
-def get_date(custom_message: str = "Insira uma data.") -> str:
-    date = prompt(custom_message)
-    date_slots = date.split("/")
-
-    ERROR_MESSAGE = ("Formato de data inválido.", "Formato correto: DD/MM/YYYY")
-
-    if len(date_slots) != 3:
-        raise ValueError(*ERROR_MESSAGE)
-
-    for i, length in enumerate([2, 2, 4]):
-        if len(date_slots[i]) != length:
-            raise ValueError(*ERROR_MESSAGE)
-
-    return date
 
 
-def get_cpf(custom_message: str = "Insira o CPF.") -> str:
-    INVALID_CPF_MSG = "Formato de CPF inválido!"
-    FORMAT_SUGGESTION_MSG = "A formatação deve ser 'DDD.DDD.DDD-DD'"
 
-    cpf = prompt(custom_message)
 
-    if len(cpf) != 14:
-        raise ValueError(
-            INVALID_CPF_MSG,
-            "Verifique se a quantidade de dígitos está correta.",
-            FORMAT_SUGGESTION_MSG,
-        )
-
-    for sep, idx in [(".", 3), (".", 7), ("-", 11)]:
-        if cpf[idx] != sep:
-            raise ValueError(
-                INVALID_CPF_MSG,
-                "Os separadores estão errados",
-                FORMAT_SUGGESTION_MSG,
-            )
-
-    cpf_digits: str = cpf.replace("-", "").replace(".", "")
-
-    if not cpf_digits.isdigit():
-        raise ValueError(
-            INVALID_CPF_MSG,
-            "Verifique a presença de letras ou outros símbolos.",
-            FORMAT_SUGGESTION_MSG,
-        )
-
-    if len(cpf_digits) != 11:
-        raise ValueError(
-            INVALID_CPF_MSG,
-            "Verifique se a quantidade de dígitos está correta.",
-            FORMAT_SUGGESTION_MSG,
-        )
-
-    return cpf
 
 
 def multiline(message: str) -> list[str]:
