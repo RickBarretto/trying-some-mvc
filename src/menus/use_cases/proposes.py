4719b65425a8e6bdfec5b347b95aafb10f528ac5
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2024-01-22 17:39:42 -0300

    refactor proposes module

--- src/menus/use_cases/proposes.py
+++ src/menus/use_cases/proposes.py
@@ -1,3 +1,5 @@
+from typing import Callable
+
 from entities.clinic import Clinic
 from entities.patient import Patient
 from entities.session import Session
@@ -5,49 +7,50 @@ from menus.use_cases import current_session_manager, patient_manager, session_ma
 import tui
 
 
-def add_to_waiting_queue(clinic: Clinic, patient: Patient) -> bool:
-    if tui.progress(f"Desejas adicionar {patient.name} à fila de espera?"):
-        current_session_manager.send_to_waiting_queue(clinic, patient)
+def _propose(question: str, use_case: Callable, *args, **kwargs) -> bool:
+    if tui.progress(question):
+        use_case(*args, **kwargs)
         return True
-
     return False
+
+
+def add_to_waiting_queue(clinic: Clinic, patient: Patient) -> bool:
+    question = f"Desejas adicionar {patient.name} à fila de espera?"
+    use_case = current_session_manager.send_to_waiting_queue
+
+    return _propose(question, use_case, clinic, patient)
 
 
 def send_patient_to_waiting_queue(clinic: Clinic, patient: Patient) -> bool:
-    if tui.progress(f"Desejas encaminhar {patient.name} para fila de espera?"):
-        current_session_manager.send_to_waiting_queue(clinic, patient)
-        return True
+    question = f"Desejas encaminhar {patient.name} para fila de espera?"
+    use_case = current_session_manager.send_to_waiting_queue
 
-    return False
+    return _propose(question, use_case, clinic, patient)
 
 
 def start_current_session(clinic: Clinic) -> bool:
-    if tui.progress("Desejas iniciar sessão atual?"):
-        current_session_manager.start(clinic)
-        return True
-
-    return False
+    question = "Desejas iniciar sessão atual?"
+    use_case = current_session_manager.start
+    
+    return _propose(question, use_case, clinic)
 
 
 def register_patient(clinic: Clinic, patient_cpf: str) -> bool:
-    if tui.progress(f"Desejas registar paciente do CPF {patient_cpf}?"):
-        patient_manager.register(clinic, patient_cpf)
-        return True
-
-    return False
+    question = f"Desejas registar paciente do CPF {patient_cpf}?"
+    use_case = patient_manager.register
+    
+    return _propose(question, use_case, clinic, patient_cpf)
 
 
 def book_session(clinic: Clinic, patient: Patient, session: Session) -> bool:
-    if tui.progress(f"Desejas agendar {patient.name} para {session.date}?"):
-        patient_manager.book_schedule(clinic, patient, session)
-        return True
+    question = f"Desejas agendar {patient.name} para {session.date}?"
+    use_case = patient_manager.book_schedule
 
-    return False
+    return _propose(question, use_case, clinic, patient, session)
 
 
 def register_session(clinic: Clinic, session_date: str) -> bool:
-    if tui.progress(f"Desejas registar sessão na data {session_date}?"):
-        session_manager.register(clinic, date=session_date)
-        return True
+    question = f"Desejas registar sessão na data {session_date}?"
+    use_case = session_manager.register
 
-    return False
+    return _propose(question, use_case, clinic, date=session_date)
