2297d12e73837aa960ab3d218a6c45d439b36cd1
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2024-01-22 19:23:02 -0300

    export multiline

--- src/tui/__init__.py
+++ src/tui/__init__.py
@@ -7,6 +7,6 @@ from .choice import choice
 from .info import info
 from .list import bullet_list
 from .progress import progress
-from .prompt import prompt
+from .prompt import prompt, multiline
 from .splash import splash
 from .warning import warn
