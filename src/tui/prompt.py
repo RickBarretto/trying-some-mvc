719d4b7044243f67509f8f6a4f4da241670894f5
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2024-01-22 18:18:05 -0300

    update prompt screen implementation
    Now it's shown in full screen as others

--- src/tui/prompt.py
+++ src/tui/prompt.py
@@ -1,6 +1,36 @@
-def prompt(msg: str) -> str:
-    print(msg)
-    return input(">>> ")
+from ._screen import Screen
+
+def prompt(ask: str, suggestion: str = "") -> str:
+    """
+    
+    Example
+    -------
+    >>> prompt("Insira o CPF do paciente.", "Formato: XXX.XXX.XXX-XX")
+    +-------------------------------------------------------+
+    |                                                       |
+    |              Insira o CPF do paciente                 |
+    |                                                       |
+    |              Formato: XXX.XXX.XXX-XX                  |
+    |                                                       |
+    +-------------------------------------------------------+
+    |      >>>|                                             |
+    """
+
+    content = [ask]
+    content.append(suggestion)
+
+    screen = Screen()
+
+    visual_contents_height = 3 # 3 = (2 horizonntal rules + 1 input line)
+    content_height = (screen.height - len(content) - visual_contents_height) >> 1 # >> 1 == // 2
+
+    screen.clear_screen()
+    screen.render_rule(position=0)
+    screen.render_vertical_space(height=content_height)
+    screen.render_content(content, center=True)
+    screen.render_vertical_space(height=content_height)
+    screen.render_rule(position=2)
+    return input(" >>> ")
 
 
 def get_date(custom_message: str = "Insira uma data.") -> str:
