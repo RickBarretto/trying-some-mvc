0dfc07d22269495caa6058b2ae8ae78be491fe9a
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2024-01-22 22:54:19 -0300

    add some common conditions to avoid code duplucation

---
+++ src/menus/use_cases/condition.py
@@ -0,0 +1,26 @@
+
+
+from entities.clinic import Clinic
+from entities.session import SessionStatus
+
+from menus.use_cases import propose, warnings
+
+import tui
+
+
+def has_active_current_session(clinic: Clinic) -> bool:
+    if clinic.current_session.status == SessionStatus.UNBEGUN:
+        warnings.session_has_never_started(clinic.current_session)
+        if not propose.start_current_session(clinic):
+            return False
+
+    if clinic.current_session.status == SessionStatus.FINISHED:
+        warnings.session_has_already_been_finished(clinic.current_session)
+        return False
+    
+    return True
+
+def has_patient_being_attended(clinic: Clinic) -> bool:
+    if not (res := clinic.current_patient):
+        tui.warn("Nenhum paciente sendo atendido.")
+    return res
