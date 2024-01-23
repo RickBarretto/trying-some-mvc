102b4be400b91e98b9c8c254f1560582077573c2
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2024-01-22 21:15:36 -0300

    refactor use case

--- src/menus/use_cases/current_session_manager/waiting_queue.py
+++ src/menus/use_cases/current_session_manager/waiting_queue.py
@@ -1,6 +1,6 @@
 from entities.clinic import Clinic
 from entities.patient import Patient
-from entities.session import SessionStatus
+from entities.session import Session, SessionStatus
 
 import tui
 
@@ -30,50 +30,64 @@ def send_to_waiting_queue(clinic: Clinic, patient: Patient | None = None) -> boo
 
     """
 
-    # Verifica status da sessão
-    current_session_status = clinic.current_session.status
+    # ============= Verifica status da sessão atual =============
 
-    if current_session_status == SessionStatus.UNBEGUN:
-        warnings.session_has_never_started(clinic.current_session)
+    if session_has_never_begun(clinic.current_session):
         if not propose.start_current_session(clinic):
             return 
 
-    if current_session_status == SessionStatus.FINISHED:
-        warnings.session_has_already_been_finished(clinic.current_session)
+    if session_has_already_finished(clinic.current_session):
         return 
 
-    # Valida a entrada do CPF
+    # ============= Pega input do usuário =============
+
     if not patient:
         cpf = request.patient_cpf()
         patient = clinic.patient_by_cpf(cpf)
 
+    # ============= Verifica registro do paciente =============
+
     if not patient:
         warnings.patient_not_registered(cpf)
-        if propose.register_patient(clinic, cpf):
-            patient = clinic.patient_by_cpf(cpf)
-        else:
-            return 
+        if not propose.register_patient(clinic, cpf):
+            return
+    
+    patient = clinic.patient_by_cpf(cpf)
 
-    # Verifica agendamento
+    # ============= Verifica agendamento do paciente =============
+
     if clinic.current_session.uid not in patient.scheduled_sessions:
         tui.warn(f"{patient.name} não está agendado para a sessão atual!")
         if not propose.book_session(clinic, patient, clinic.current_session):
-            return 
+            return
 
-    # Verifica se o paciente já consta na fila de espera
-    waiting_queue = clinic.waiting_queue
-    if patient.uid in waiting_queue:
+    # ============= Verifica se paciente está na fila =============
+        
+    if patient.uid in clinic.waiting_queue:
         tui.warn("Paciente já está na fila de espera.")
-        return 
+        return
 
-    # Põe o paciente na fila de espera
+    # ============= Põe paciente na fila =============
+
     clinic.waiting_queue.append(patient.uid)
 
-    # Feedback
+    # ============= Feedback =============
 
-    tui.info(
-        [
-            f"{patient.name} colocado na fila de espera,",
-            f"O mesmo se encontra na posição {len(waiting_queue)}!",
-        ]
-    )
+    message = [
+        f"{patient.name} colocado na fila de espera,",
+        f"O mesmo se encontra na posição {len(clinic.waiting_queue)}!",
+    ]
+
+    tui.info(message)
+
+def session_has_already_finished(session: Session):
+    if res := (session.status == SessionStatus.FINISHED):
+        warnings.session_has_never_started(session)
+
+    return res
+
+def session_has_never_begun(session: Session) -> bool:
+    if res := (session.status == SessionStatus.UNBEGUN):
+        warnings.session_has_already_been_finished(session)
+
+    return res
