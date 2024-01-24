ac7ff2cb9cf337b63f21d3afcf3a5430d55fa97b
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2024-01-24 15:50:41 -0300

    use-case\finish: refactor

--- src/menus/use_cases/current_session_manager/finish.py
+++ src/menus/use_cases/current_session_manager/finish.py
@@ -22,19 +22,25 @@ def finish(clinic: entities.Clinic, suppress_warnings: bool = False):
     session_status = clinic.current_session.status
 
     if not suppress_warnings:
-        if session_status == entities.SessionStatus.UNBEGUN:
+    
+        has_not_begun     = session_status == entities.SessionStatus.UNBEGUN
+        has_been_finished = session_status == entities.SessionStatus.FINISHED
+        
+        if has_not_begun:
             warnings.session_has_never_started(clinic.current_session)
             if not tui.proceed("Desejas finalizar mesmo assim?"):
                 return
-
-        if session_status == entities.SessionStatus.FINISHED:
+            
+        if has_been_finished:
             warnings.session_has_already_been_finished(clinic.current_session)
             return
 
     # ============= Desativa sessão atual =============
 
+    _finish_current_session(clinic)
+
+
+def _finish_current_session(clinic: entities.Clinic):
+    """Finaliza a sessão atual"""
     clinic.current_session.status = entities.SessionStatus.FINISHED
-
-    # ============= Feedback =============
-
     tui.info(f"Sessão {clinic.current_session.date} finalizada!")
