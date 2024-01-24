9c1d7182bd6cd5711af1fbb5f2b79d5a3d17741f
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2024-01-23 21:52:09 -0300

    menu\startup: add docs

--- src/menus/start.py
+++ src/menus/start.py
@@ -6,10 +6,40 @@ import tui
 
 __all__ = ["start_application"]
 
+# ============= Menu Inicial =============
+
+def start_application(clinic: Clinic):
+    """Menu inicial da aplicação.
+    
+    Esse menu é apresentado uma única vez ao usuário, 
+    que irá registrar uma primeira sessão e loggar na mesma, 
+    e então ser encaminhado para o ``main_menu``.
+
+    Arguments
+    ---------
+    clinic: Clinic
+        Recebe uma instância de ``Clinic``. 
+        Lembrando que esta deve ser um singleton, e, 
+        portanto ter uma, e apenas uma única instância da mesma.
+    """
+    # Apresentação
+    tui.splash(["Bem-vindo ao Dente Clean Manager!", "", "Pressione [Enter] para iniciar uma sessão"])
+
+    # Atualiza a sessão
+    current_session_manager.update(clinic)
+    
+    # Redirecionamento
+    main_menu(clinic)
+
+
+# ============= Menu Principal =============
 
 def main_menu(clinic: Clinic):
-
-    current_session_manager.update(clinic)
+    """Menu principal do usuário.
+    
+    Onde o usuário poderá gerir a sessão atual (Finalizar ou Atualizar),
+    e ter acesso aos menus de recepção e dentista.
+    """
 
     options = [
             ("Iniciar como recepção", reception.start),
@@ -19,7 +49,3 @@ def main_menu(clinic: Clinic):
         ]
     
     MainMenu(clinic, options).run()
-
-def start_application(clinic: Clinic):
-    tui.splash(["Bem-vindo ao Dente Clean Manager!", "", "Pressione [Enter] para iniciar uma sessão"])
-    main_menu(clinic)
