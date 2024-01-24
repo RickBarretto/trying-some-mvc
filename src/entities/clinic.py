af8719ba4693f41e0fba4ef1aec9dd7a8b3c9b09
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2024-01-23 21:26:11 -0300

    model\clinic: add docs

--- src/entities/clinic.py
+++ src/entities/clinic.py
@@ -3,6 +3,28 @@ from .patient import Patient
 
 
 class Clinic:
+    """Define o modelo da Clínica
+    
+    Attributes
+    ----------
+    current_session: Session | None = None
+        Define a sessão atual, inicialmente sendo None
+    current_patient: Patient | None = None
+        Define o paciente atual, inicialmente sendo None
+    waiting_queue: list[int]
+        A fila de espera é uma lista com os IDs dos pacientes (``Patient.uid``)
+
+    sessions: list[Session]
+        Lista de sessões registradas no banco de dados
+    patients: list[Patient]
+        Lista de pacientes registrados no banco de dados
+
+    last_session_id: int = 0
+    last_patient_id: int = 0
+        Armazena o último ID gerado para o banco de dados 
+        das sessões e pacientes, respectivamente
+
+    """
     def __init__(self) -> None:
         self.current_session: Session | None = None
         self.current_patient: Patient | None = None
@@ -15,9 +37,11 @@ class Clinic:
         self.last_patient_id = 0
 
     def new_session_id(self):
+        """Gera um novo ID para sessão"""
         return self.last_session_id + 1
 
     def new_patient_id(self):
+        """Gera um novo ID para paciente"""
         return self.last_patient_id + 1
 
     def session_by_date(self, date: str) -> Session | None:
