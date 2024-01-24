22de1d208e3c4b6cf9cd1ffe3849bfddb847984d
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2024-01-24 19:38:41 -0300

    add __all__

--- src/tui/__init__.py
+++ src/tui/__init__.py
@@ -9,3 +9,14 @@ from .list import bullet_list
 from .proceed import proceed
 from .prompt import prompt, multiline
 from .splash import splash
+
+__all__ = [
+    "choice",
+    "bullet_list",
+    "info",
+    "multiline",
+    "proceed",
+    "prompt",
+    "splash",
+    "warn"
+]
