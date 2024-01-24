3b0741cf269eeb73f7fa88fa6f9cc88fdd7df50b
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2024-01-24 17:59:36 -0300

    add docs to new package

---
+++ src/menus/use_cases/commons/__init__.py
@@ -0,0 +1,21 @@
+"""Casos de usos menores comuns entre os casos de uso principais
+
+Modules
+-------
+condition
+    Funções comuns que retornam ``bool``, ao validar certas condições.
+
+fetch
+    Funções comuns que retornam um Modelo buscado no banco de dados.
+
+propose
+    Funções comuns que propõe formas de resolução de algum problema ao usuário.
+    Retornam ``bool`` de acordo com a escolha do usuário.
+
+request
+    Funções comuns que pedem e validam uma entrada do usuário.
+
+warnings
+    Funções comuns que avisam ao usuário sobre alguma coisa do sistema.
+
+"""
