64bce54e363fe09b9224a3806f6b6d763834286c
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2024-01-24 14:25:33 -0300

    usecases\conditions: add docs

--- src/menus/use_cases/condition.py
+++ src/menus/use_cases/condition.py
@@ -1,3 +1,9 @@
+"""Definem as condições comuns das regras de negócio
+
+Algumas dessas condições incluem propostas ao usuário para contornar
+o problema lidado.
+"""
+
 from entities.clinic import Clinic
 from entities.session import SessionStatus
 
@@ -7,15 +13,32 @@ import tui
 
 
 def is_able_to_read_records(clinic: Clinic) -> bool:
+    """Retorna se o médico é capaz de ler o prontuário.
+    
+    Um médico é capaz de ler o prontuário se e somente se:
+        1. A sessão já foi iniciada
+        2. Tem um paciente sendo atendido.
+    """
     return has_patient_being_attended(clinic) and has_active_current_session(clinic)
 
 
 def has_active_current_session(clinic: Clinic) -> bool:
+    """Retorna se a sessão atual está ativa ou não.
+    
+    Warnings                        Proposes
+    --------                        --------
+    * Sessão nunca fora iniciada    * Propõe iniciá-la
+    * Sessão já finalizada
+
+    """
+
+    # Caso a sessão não tenha sido iniciada ainda, propõe iniciá-la.
     if clinic.current_session.status == SessionStatus.UNBEGUN:
         warnings.session_has_never_started(clinic.current_session)
         if not propose.start_current_session(clinic):
             return False
 
+    # Caso a sessão esteja finalizada, a leitura não pode ser mais feita.
     if clinic.current_session.status == SessionStatus.FINISHED:
         warnings.session_has_already_been_finished(clinic.current_session)
         return False
@@ -24,6 +47,12 @@ def has_active_current_session(clinic: Clinic) -> bool:
 
 
 def has_patient_being_attended(clinic: Clinic) -> bool:
+    """Retorna se há um paciente sendo atendido.
+    
+    Warnings
+    --------
+    * Nenhum paciente sendo atendido
+    """
     if not (res := clinic.current_patient):
         tui.warn("Nenhum paciente sendo atendido!")
     return bool(res)
