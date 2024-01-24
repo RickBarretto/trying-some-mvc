49063a817479d5d47f459d1e1012ac8edade98ea
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2024-01-24 11:25:24 -0300

    tui\utils: add docs

--- src/tui/_utils.py
+++ src/tui/_utils.py
@@ -44,28 +44,51 @@ def render_rule(inner_width: int, position: int = 0):
 
 
 def render_default_content(content: str):
+    """Renderiza o conteúdo, sem alinhamento."""
     vertical_bar = "│"  # unicode: 2502
     print(vertical_bar, content, vertical_bar, sep="")
 
 
 def render_content(content: str, inner_width: int, center: bool = False):
+    """Renderiza o conteúdo alinhando-o.
+    
+    Arguments
+    ---------
+    content: str
+        O conteúdo em si
+    inner_width: int
+        Define o espaço interno do conteúdo
+    center: bool = False
+        Determina se o conteúdo será centralizado ou não.
+    """
     # define o posicionamento de ``content`` de acordo com ``center``
     pos = "^" if center else "<"
     tab = left_align_width() if not center else 0
 
     for line in content:
         # Imprime "|        <conteúdo>      |" para ``center = True``
+        # ou
         # Imprime "|<conteúdo>              |" para ``center = False``
         line = line.rstrip()
         prefix = " " * tab
         render_default_content(f"{prefix}{line:{pos}{inner_width - tab}}")
 
 
-def left_align_width():
+def left_align_width() -> int:
+    """Retorna o espaço necessário para a margem esquerda."""
     return int(os.get_terminal_size().columns * 0.2)
 
 
 def render_vertical_space(inner_width: int, height: int):
+    """Renderiza um espaço vertical.
+    
+    Arguments
+    ---------
+    inner_width: int
+        O tamanho interno desse espaço
+    height: int
+        A altura desse espaço
+    """
     for _ in range(height - 1):
         # Imprime "|        <espaço em branco>      |"
         render_default_content(" " * inner_width)
